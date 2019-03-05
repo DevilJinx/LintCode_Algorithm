2d7dce2db9f85f7485270dfa4455aaac51f4fffe
DevilJinx <jxs66@163.com>
2019-03-05 16:16:41 +0800

    将一个字符由小写字母转换为大写字母

---
+++ 0145_Lowercase to Uppercase.py
@@ -0,0 +1,20 @@
+# coding=utf-8
+'''
+描述：将一个字符由小写字母转换为大写字母
+样例：
+    样例 1:
+        输入: 'a'
+        输出: 'A'
+    样例 2:
+        输入: 'b'
+        输出: 'B'
+'''
+
+class Solution:
+    """
+    @param character: a character
+    @return: a character
+    """
+    def lowercaseToUppercase(self, character):
+        # write your code here
+        return (character.upper())
